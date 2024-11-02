5e08e265d8e71da7888375c2ad22dcd6d71656c7
Yesid Casallas <138601965+SuitsG@users.noreply.github.com>
2024-11-02 16:32:48 -0500

    avaces chorono

---
+++ index.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Chrono</title>
+    <link rel="stylesheet" href="/css/style.css">
+</head>
+
+<body>
+    <header class="header">
+        <h1 class="header__h1">INDUSTRIA CHRONO</h1>
+    </header>
+    <main class="main">
+        <section class="main__section">
+            <a href="/sections/create.php" class="main__section__a">
+                <div class="main__section__a__div">
+                    <img src="/assets/create.jpg" alt="" class="main__section__a__div__img">
+                    <p class="main__section__a__div__p">Create (crear)
+                        Dentro de este módulo se podrán crear nuevos usuarios (empleados).</p>
+                </div>
+            </a>
+            <a href="/sections/delete.php" class="main__section__a">
+                <div class="main__section__a__div">
+                    <img src="/assets/delete.jpg" alt="" class="main__section__a__div__img">
+                    <p class="main__section__a__div__p">Delete (eliminar)
+                        Dentro de este módulo se podrán eliminar permanentemente usuarios (empleados).</p>
+                </div>
+            </a>
+            <a href="/sections/update.php" class="main__section__a">
+                <div class="main__section__a__div">
+                    <img src="/assets/update.jpg" alt="" class="main__section__a__div__img">
+                    <p class="main__section__a__div__p">Actualización de datos
+                        Dentro de este módulo se podrán realizar cambios sobre la base de datos.</p>
+                </div>
+            </a>
+            <a href="/sections/consultation.php" class="main__section__a">
+                <div class="main__section__a__div">
+                    <img src="/assets/consult.jpg" alt="" class="main__section__a__div__img">
+                    <p class="main__section__a__div__p">Consultas de datos
+                        Dentro de este módulo se podrán consultar fechas de cumpleaños, salarios, cantidad de empleados, etc.
+                    </p>
+                </div>
+            </a>
+        </section>
+    </main>
+    <footer class="footer">
+    <p>Laura es una chinita consentida por mami</p>
+    </footer>
+</body>
+
+</html>
